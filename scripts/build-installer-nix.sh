122f4459b6869e7665df1183a846beb692ac4e96
Michael Bishop <michael.bishop@iohk.io>
2018-04-12 08:32:21 -0300

    enable devnet on buildkite

--- scripts/build-installer-nix.sh
+++ scripts/build-installer-nix.sh
@@ -21,3 +21,9 @@ nix-build release.nix -A staging.installer --argstr buildNr $BUILDKITE_BUILD_NUM
 if [ -n "${BUILDKITE_JOB_ID:-}" ]; then
   buildkite-agent artifact upload result/Daedalus*installer*.bin --job $BUILDKITE_JOB_ID
 fi
+
+echo '~~~ Building devnet installer'
+nix-build release.nix -A devnet.installer --argstr buildNr $BUILDKITE_BUILD_NUMBER --argstr version $VERSION
+if [ -n "${BUILDKITE_JOB_ID:-}" ]; then
+  buildkite-agent artifact upload result/Daedalus*installer*.bin --job $BUILDKITE_JOB_ID
+fi
